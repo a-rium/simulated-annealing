f3fe638ed148e214aefb5add7275d2a6752c7514
a-rium <gottacatchmall@gmail.com>
2020-09-01 21:45:54 +0200

    Adattato il codice relativo al problema delle regine, implementato argparse

--- queens-main.py
+++ queens-main.py
@@ -3,9 +3,17 @@ from search import *
 
 import sys
 import time
+import argparse
+
+
+class QueensProblemSchedule(LogSchedule):
+	def reschedule(self, tries):
+		print("Temperature has risen")
+		self.temperature += 1
 
 	
-def print_checkboard(n, queens):
+def print_checkboard(queens):
+	n = len(queens)
 	empty = " "
 	queen_marker = "X"
 	hline = "+" + ("-+" * n)
@@ -18,20 +26,28 @@ def print_checkboard(n, queens):
 	print(hline)
 
 
-def process_arguments(args):
-	if len(args) < 2:
-		print("Utilizzo: passare la lunghezza del lato della scacchiera 'n' (n > 3)")
+def value_or_none_if_raises(f, *args, **kwargs):
+	try:
+		return f(*args, **kwargs)
+	except:
 		return None
-	else:
-		try:
-			n = int(sys.argv[1])
-			if n > 3:
-				return {"n": n}
-		except:
-			pass
 
-	print("Errore: n deve essere un numero intero positivo maggiore di 3")
-	return None
+
+def process_arguments(args):
+	parser = argparse.ArgumentParser()
+	parser.add_argument("n", type=int)
+	parser.add_argument("-t", default=20, type=float)
+	parser.add_argument("-c", default=10, type=float)
+	parser.add_argument("--freezing-at", default=0.1, dest="freezing_at", type=float)
+	parser.add_argument("--transitions-per-temperature", default=1, dest="transitions_per_temperature", type=float)
+	parser.add_argument("--max-iterations", default=2**32, dest="max_iterations", type=int)
+	inputs = parser.parse_args()
+	if inputs.n < 4:
+		print("Il numero di regine deve essere maggiore di 3")
+	elif inputs.t <= 0:
+		print("Initial temperature must be positive")
+	else:
+		return inputs
 
 
 def main():
@@ -39,43 +55,27 @@ def main():
 	if inputs is None:
 		return
 
-	n = inputs['n']
-	
-	t0 = 20
-	freezing_at = 0.1
-	c = 10
-
-	schedule = log_schedule(t0, c)
-	annealing_options = {
-		"max_iterations": 2**32,
-		"transitions_per_temperature": 1,
-		"freezing_at": freezing_at
+	options = {
+		"max_iterations": inputs.max_iterations,
+		"transitions_per_temperature": inputs.transitions_per_temperature,
+		"freezing_at": inputs.freezing_at
 	}
 
-	print(f"Temperatura iniziale di annealing: {t0}")
+	print(f"Temperatura iniziale di annealing: {inputs.t}")
 
-	tries = 0
-	start = time.time()
-	while True:
-		tries += 1
-		print(f"\rTentativo numero {tries}", end="")
+	make_problem = lambda: QueensProblem.random(inputs.n)
+	schedule = QueensProblemSchedule(inputs.t, inputs.c)
+	solution, elapsed = solve_by_simulated_annealing(make_problem, schedule, **options)
+		
+	print("Trovata una soluzione!")
+	print("===== Rappresentazione del problema delle n regine =====")
+	print(solution.board)
+	print("===== Rappresentazione della scacchiera =====")
+	print_checkboard(solution.board)
 
-		problem = QueensProblem.random(n)
-		solution = simulated_annealing(problem, schedule, **annealing_options)
-
-		if solution is not None:
-			if problem.goal(solution):
-				end = time.time()
-				print()
-				print("Trovata una soluzione!")
-				print("===== Rappresentazione del problema delle n regine =====")
-				print(solution.board)
-				print("===== Rappresentazione della scacchiera =====")
-				print_checkboard(n, solution.board)
-				break
-
-	timestamp = time.strftime("%H:%M:%S", time.gmtime(end - start))
+	timestamp = time.strftime("%H:%M:%S", time.gmtime(elapsed))
 	print(f"Tempo impiegato: {timestamp}")
+
 
 
 if __name__ == "__main__":
